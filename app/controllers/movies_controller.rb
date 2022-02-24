ebc96b4086c0b5147869b5da870e5637d327f47c
Bikash Shrestha <bikashshrestha@bajratechnologies.com>
2022-02-24 16:34:46 +0545

    feat: generate controller for movies

---
+++ app/controllers/movies_controller.rb
@@ -0,0 +1,10 @@
+class MoviesController < ApplicationController
+  def index
+  end
+
+  def create
+  end
+
+  def show
+  end
+end
---
+++ spec/requests/movies_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.describe "Movies", type: :request do
+  describe "GET /index" do
+    it "returns http success" do
+      get "/movies/index"
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET /create" do
+    it "returns http success" do
+      get "/movies/create"
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET /show" do
+    it "returns http success" do
+      get "/movies/show"
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+end
